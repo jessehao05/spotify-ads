d42f28b72364f42d44ef1b4ad935818d4dd89638
Jesse Hao <jehyu77@gmail.com>
2025-02-20 22:29:50 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spotify Ad Script
+
+Mutes Spotify in the Windows volume mixer when an ad plays and unmutes when a song comes back on.
---
+++ testing/pycaw_test.py
@@ -0,0 +1,26 @@
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume, ISimpleAudioVolume
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+current = volume.GetMasterVolumeLevel()
+print(current)
+
+# SetMute(): 1 for mute, 0 for unmute
+
+sessions = AudioUtilities.GetAllSessions()
+
+
+for session in sessions:
+    volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+
+
+    # if session.Process:
+    #     print(session.Process.name())
+
+    if session.Process and session.Process.name() == "Spotify.exe":
+        volume.SetMute(0, None)    
+    
